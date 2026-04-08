b385ec6ac4dae1309dca15e90ef9f8007b7e1550
Viktor <lytvynenkobooks@gmail.com>
2026-04-08 10:50:41 +0300

    server

--- routes/products.js
+++ routes/products.js
@@ -6,9 +6,11 @@ const products = [
     { id: 1, name: "iPhone 15", price: 1000 }
 ];
 
-
 router.get("/", (req, res) => {
-    res.json(products);
+    res.status(200).json({
+        success: true,
+        data: products
+    });
 });
 
 export default router;
