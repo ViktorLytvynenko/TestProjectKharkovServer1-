7e7c37a201248174612f015cfe15c48716385aba
Viktor <lytvynenkobooks@gmail.com>
2026-04-09 11:25:22 +0300

    socket

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 import productsRouter from "./routes/products.js";
 import arrivalsRouter from "./routes/arrivals.js";
 import usersRouter from "./routes/users.js";
@@ -15,9 +18,33 @@ app.use("/arrivals", arrivalsRouter);
 app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {
-    res.json({message: "API is running"});
+    res.json({ message: "API is running" });
 });
 
-app.listen(PORT, () => {
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+
+let activeSessions = 0;
+
+io.on("connection", (socket) => {
+    activeSessions++;
+
+    io.emit("sessions", activeSessions);
+
+    console.log("User connected", activeSessions);
+
+    socket.on("disconnect", () => {
+        activeSessions--;
+        io.emit("sessions", activeSessions);
+        console.log("User disconnected", activeSessions);
+    });
+});
+
+httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
